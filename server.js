1a9c3f44475bbffd51ce8e16d5e9c9537fa6fc8c
Charles Albert <charles.ccomp@gmail.com>
2023-04-18 08:52:03 -0300

    implementada comunicação básica, implementando mensagem de status

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+app.use(express.static("public"))
+
+const http = require("http").createServer(app)
+
+//const newServer = require("socket.io")
+//const serverSocket = newServer()
+const serverSocket = require("socket.io")(http)
+
+const PORT = 6969
+http.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`))
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"))
+
+serverSocket.on('connection', (socket) => {
+    console.log(`Cliente ${socket.id} conectou`)
+
+    socket.on('message', (texto) => {
+        //$("#mensagens").val(`${socket.id} diz: ${texto}`)
+        console.log(`${socket.id} diz: ${texto}`)
+        var mensagem = `${socket.id} diz: ${texto}`
+        serverSocket.emit("message", mensagem)
+    })
+
+    socket.on('status', (status) => {
+        var statusText = `${socket.id} está digitando`
+        if(status) {
+            console.log(statusText)
+            serverSocket.emit("status", statusText)
+        }
+    })
+})
